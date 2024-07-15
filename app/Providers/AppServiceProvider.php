f1bf509a4d2d61ab17ee1c5074f8c0b932bb1e6d
Nikudo100 <kunachillo@gmail.com>
2024-07-15 18:39:55 +0300

    add breeze:install for auth

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,8 @@
 
 namespace App\Providers;
 
+use Illuminate\Auth\Notifications\ResetPassword;
 use Illuminate\Support\ServiceProvider;
-use App\Services\WbApiService;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -12,9 +12,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        $this->app->singleton('wbapi', function ($app) {
-            return new WbApiService();
-        });
+        //
     }
 
     /**
@@ -22,6 +20,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        ResetPassword::createUrlUsing(function (object $notifiable, string $token) {
+            return config('app.frontend_url')."/password-reset/$token?email={$notifiable->getEmailForPasswordReset()}";
+        });
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,10 @@
-<?
+<?php
 
+use Illuminate\Http\Request;
+use App\Http\Controllers\TestController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\StockController;
-use App\Http\Controllers\AbcReportController;
-use App\Http\Controllers\TurnoverReportController;
-use App\Http\Controllers\ClusterController;
+Route::get('/test', [TestController::class, 'index'])->middleware('role:User');
 
-Route::get('/stocks', [StockController::class, 'index']);
-Route::get('/abc-reports', [AbcReportController::class, 'index']);
-Route::get('/turnover-reports', [TurnoverReportController::class, 'index']);
-Route::get('/clusters', [ClusterController::class, 'index']);
+Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
+    return $request->user();
+});
